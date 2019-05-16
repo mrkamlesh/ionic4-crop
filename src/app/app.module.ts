9e9d508b7daf74852f5b18615e473fce339a4297
Kamlesh Kumar <kamlesh.kumar@agreeya.com>
2019-05-16 11:23:18 +0530

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { ImagePicker } from '@ionic-native/image-picker/ngx';
+import { Crop } from '@ionic-native/crop/ngx';
+import { FileTransfer } from '@ionic-native/file-transfer/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    ImagePicker,
+    Crop,
+    File,
+    FileTransfer,
+    WebView
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { Crop } from '@ionic-native/crop/ngx';
+import { ImagePicker } from '@ionic-native/image-picker/ngx';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer/ngx';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  fileUrl: any = null;
+  respData: any;
+
+  constructor(private imagePicker: ImagePicker,
+    private crop: Crop,
+    private webview: WebView,
+    private transfer: FileTransfer) { }
+
+  cropUpload() {
+    this.imagePicker.getPictures({ maximumImagesCount: 1, outputType: 0 }).then((results) => {
+      for (let i = 0; i < results.length; i++) {
+        console.log('Image URI: ' + results[i]);
+        this.crop.crop(results[i], { quality: 100 })
+          .then(
+            newImage => {
+              console.log('new image path is: ' + newImage);
+              const fileTransfer: FileTransferObject = this.transfer.create();
+              const uploadOpts: FileUploadOptions = {
+                fileKey: 'file',
+                fileName: newImage.substr(newImage.lastIndexOf('/') + 1)
+              };
+
+              // this.fileUrl = newImage;
+              console.log("pre: ", newImage);
+              this.fileUrl = this.webview.convertFileSrc(newImage);
+              console.log("after: ", this.fileUrl);
+              // fileTransfer.upload(newImage, 'http://192.168.0.7:3000/api/upload', uploadOpts)
+              //   .then((data) => {
+              //     console.log(data);
+              //     this.respData = JSON.parse(data.response);
+              //     console.log(this.respData);
+              //     this.fileUrl = this.respData.fileUrl;
+              //   }, (err) => {
+              //     console.log(err);
+              //   });
+            },
+            error => console.error('Error cropping image', error)
+          );
+      }
+    }, (err) => { console.log(err); });
+  }
+
+}
